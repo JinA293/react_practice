be364b370af79fa01c7b505fc4d33e9a0a50edee
DESKTOP-I5BEVNJ\jin_h <jin.codegym1217@gmail.com>
2022-09-01 23:43:47 +0900

    日付を取ってくる練習

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+let dates = [];
+let betweenDates = 0;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const getDatesBetweenDates = (startDate, endDate) => {
+
+  //to avoid modifying the original date
+  const theDate = new Date(startDate)
+  while (theDate < endDate) {
+    dates = [...dates, new Date(theDate)]
+    theDate.setDate(theDate.getDate() + 1)
+    betweenDates++
+  }
+  return console.log(dates, betweenDates);
 }
 
-export default App;
+const today = new Date()
+const oneMonthFromNow = new Date(today)
+oneMonthFromNow.setDate( oneMonthFromNow.getDate() + 31)
+getDatesBetweenDates(today, oneMonthFromNow)
+
+for (let i = 1; i <= betweenDates + 1; i++){
+  let year = dates[i].getFullYear();
+  let month = dates[i].getMonth() + 1;
+  let day = dates[i].getDate();
+  console.log( i + "は" + year + '年' + month + '月' + day + '日');
+}
+
